504c99603d5e3b6e98c2d679bddeb2754e28f9d3
Alexander940 <alexanderecheverry@outlook.es>
2023-10-14 22:27:23 -0500

    final delivery

--- src/main/java/com/library/management/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.library.management.service;
-
-import com.library.management.persistence.model.LoginRequest;
-import com.library.management.persistence.model.LoginResponse;
-
-public interface IUserService {
-
-    LoginResponse getUserByNameAndPassword(final LoginRequest loginRequest) throws Exception;
-}
